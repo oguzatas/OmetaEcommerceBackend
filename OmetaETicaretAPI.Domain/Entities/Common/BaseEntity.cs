0e39400b8c53860575d08aefc93347071e943193
oguzatas <oguzhanatas37@gmail.com>
2023-12-27 16:33:03 +0300

    Interceptor

--- OmetaETicaretAPI.Domain/Entities/Common/BaseEntity.cs
+++ OmetaETicaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -12,7 +12,7 @@ namespace OmetaETicaretAPI.Domain.Entities
 
 		public DateTime CreateDate { get; set; } = DateTime.Now;
 
-		public DateTime ChangeTime { get; set; }
+		public DateTime ChangeDate { get; set; }
 
 
 	}
--- OmetaEticaretAPI.Persistence/Contexts/ECommerceAPIDbContext.cs
+++ OmetaEticaretAPI.Persistence/Contexts/ECommerceAPIDbContext.cs
@@ -19,5 +19,21 @@ namespace OmetaEticaretAPI.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+
+		// interceptor
+		public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			var datas = ChangeTracker.Entries<BaseEntity>();
+
+			foreach (var data in datas)
+			{
+				_  = data.State switch
+				{
+					EntityState.Added => data.Entity.CreateDate = DateTime.UtcNow,
+					EntityState.Modified => data.Entity.ChangeDate = DateTime.UtcNow
+				};
+			}
+			return await base.SaveChangesAsync(cancellationToken);
+		}
 	}
 }
