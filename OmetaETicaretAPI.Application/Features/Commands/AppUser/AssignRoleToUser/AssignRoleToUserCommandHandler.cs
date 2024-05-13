710b994dc6a69d4131bd039e097adcfc2d444775
oguzatas <oguzhanatas37@gmail.com>
2024-05-13 18:48:48 +0300

    cqrs, role assignments

---
+++ OmetaETicaretAPI.Application/Features/Commands/AppUser/AssignRoleToUser/AssignRoleToUserCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmetaETicaretAPI.Application.Features.Commands.AppUser.AssignRoleToUser
+{
+    public class AssignRoleToUserCommandHandler : IRequestHandler
+    {
+    }
+}
